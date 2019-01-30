4a28c45be9bb1c2596c704d0e1589f2bcc88f463
Mark Lee <mark.lee@capricasoftware.co.uk>
2019-01-30 08:36:10 +0000

    Add OSX stubs to support native full-screen

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# vlcj-osx-stubs
-Various stubs for OSX-specific classes used to cross-compile vlcj on Linux or Windows
+
+vlcj-osx-stubs
+==============
+
+This small library provides stub implementations of various OSX classes that can be used to cross-compile vlcj on Linux
+and Windows.
+
+These classes are used to implement the native full-screen strategy on OSX by using the Apple EAWT classes.
+
+The library is only required at compile-time, and should ordinarily *not* be distributed with your application.
---
+++ src/main/java/com/apple/eawt/Application.java
@@ -0,0 +1,38 @@
+/*
+ * This file is part of VLCJ.
+ *
+ * VLCJ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VLCJ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VLCJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2009-2019 Caprica Software Limited.
+ */
+
+package com.apple.eawt;
+
+import java.awt.*;
+
+public final class Application {
+
+    private static final Application APPLICATION = new Application();
+
+    public static Application getApplication() {
+        return APPLICATION;
+    }
+
+    public void requestToggleFullScreen(Window window) {
+    }
+
+    private Application() {
+    }
+
+}
